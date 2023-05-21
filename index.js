49295b021330f88fe7f4e549440b5863d60e3530
Hana <grace.sambora@gmail.com>
2023-05-20 23:40:33 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(){
+    //console.log("The DOM has loaded");
+    let newText = document.createElement('p');
+    //console.log(newText);
+    newText.textContent = 'This is really cool!';
+    document.querySelector('#text').remove();
+    document.querySelector('body').appendChild(newText);
+})
+
+//console.log("The DOM hasn't loaded")
